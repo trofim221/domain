5cd3854d09fe4a2945a1801cb6db436b158491b0
trofim221 <trofim221@gmail.com>
2020-05-12 15:11:58 +0300

    verify domain v.3 in file domain.php

--- domain.php
+++ domain.php
@@ -1,7 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: trofim
- * Date: 12.05.2020
- * Time: 2:58 PM
- */
+
+class subDomain
+{
+    public function extractDomain($domain)
+    {
+        if(preg_match("/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i", $domain, $matches))
+        {
+            return $matches['domain'];
+        } else {
+            return $domain;
+        }
+    }
+
+    public function extractSubDomains($domain)
+    {
+        $sub_domains = $domain;
+        $domain = $this->extractDomain($sub_domains);
+
+        $sub_domains = rtrim(strstr($sub_domains, $domain, true), '.');
+
+        if($sub_domains == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+$domainArray = array(
+    'semalt.net.ua',
+    'sub1.semalt.net.ua',
+    'sub1.semalt.net',
+    'sub1.sub2.semalt.net',
+    'sub1.sub2.sub3.semalt.net'
+);
+
+$extract_domain = new subDomain();
+
+foreach($domainArray as $k => $v)
+{
+    echo $k." => ".$extract_domain->extractSubDomains($v)."<br>";
+}
+
